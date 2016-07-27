542cc6699613d406b76c007971d2cf5cc42c843a
Walker Langley <walkerlangley@gmail.com>
2016-07-26 23:03:12 -0400

    imported Path in webpack and am using path.join to define output path.  Also updated eslintrc

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,33 @@
+var webpack = require('webpack');
+var path = require('path');
+
 module.exports = {
-  entry: './index.js',
+  devtool: 'eval',
+  entry: [
+    'webpack-dev-server/client?http://localhost:3333',
+    'webpack/hot/only-dev-server',
+    './src/index'
+  ],
   output: {
-    path: './',
-    filename: 'index.js'
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
   },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
   devServer: {
     inline: true,
     port:3333
   },
   module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel',
-        query: {
-          presets: ['es2015', 'react']
-        }
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['es2015', 'react']
       }
-    ]
+    }]
   }
 }
